9beec37b54de9ce4663325fc73627b9f561ad773
Shumakov Ilya <shumakoff.ilya@yandex.ru>
2026-02-19 20:08:40 +0300

    feat: products with items

--- src/main/java/com/example/product/ProductRepository.java
+++ src/main/java/com/example/product/ProductRepository.java
@@ -1,7 +1,20 @@
 package com.example.product;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @Query("""
+    SELECT p FROM Product p
+    JOIN FETCH p.orderItems oi
+    GROUP BY p.id, p.name, p.category, p.price, p.stockQuantity
+    ORDER BY SUM(io.quantity) DESC)
+    """)
+    List<Product> getTopSellingProducts(Pageable pageable);
+
+    List<Product> findAllByStockQuantityLessThanEqual(Integer stockQuantity);
 }
